03681d1e77a91807ac8d02d8cc44dd72c5df5ded
Lawrence Tay <145833781+ninja-lawrence@users.noreply.github.com>
2025-11-14 12:47:21 +0000

    Add user log functionality with view and delete options

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 import joblib
 import os
+import sqlite3
+import datetime
 
 from groq import Groq
 # get key from .env file
@@ -51,6 +53,31 @@ def llama_result():
     ])
     r = r.choices[0].message.content
     return render_template("llama_result.html", r=r)
+
+
+@app.route("/userlog",methods=["GET","POST"])
+def userlog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('''select *
+    from user''')
+    r=""
+    for row in c:
+        print(row)
+        r = r + str(row)
+    c.close()
+    conn.close()
+    return(render_template("userlog.html",r=r))
+
+@app.route("/deletelog",methods=["GET","POST"])
+def deletelog():
+    conn = sqlite3.connect("user.db")
+    c = conn.cursor()
+    c.execute('DELETE FROM user',);
+    conn.commit()
+    c.close()
+    conn.close()
+    return(render_template("deletelog.html"))
 
 if __name__ == "__main__":
     app.run(debug=True)
